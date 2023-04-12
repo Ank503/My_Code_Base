9effd7a5dbde83a2672142117610b4e5bc9b0e00
Ankit kumar <ankit88741@gmail.com>
2023-04-12 12:16:53 -0700

    first file uploaded

---
+++ shell_scripts/first.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# Variables are of two types:
+# First is System variable which is defined by os
+# It is defined in capital letters only
+#echo $BASH
+#echo $BASH_VERSION 
+#echo $HOME
+#echo $PWD
+#### User Defined variables ###
+# User Defined variables can be drfined in capital also
+# variable name should not be start with number
+name=Ankit
+age=22
+age=24 # changing the value of variable
+echo Hi my Name is $name
+echo I am $age years old
+
