e720f84247f5283eeb707e951b573460e9950af4
Ankit kumar <ankit88741@gmail.com>
2023-06-22 16:34:19 +0530

    Added a commented line

--- shell_scripts/local_var.sh
+++ shell_scripts/local_var.sh
@@ -13,4 +13,4 @@ echo $name:After
 # All variables are global in shell scripts.
 # here name is a global variable and can be accessed anywhere in script.
 # if we want that the variable of a function should not change or it remain local we have to use local.
-
+# When we use local variable it can be accessed from inside the function only.
