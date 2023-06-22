921939aa8bd54905cac3685bf16c83fe926856de
Ankit kumar <ankit88741@gmail.com>
2023-06-22 16:31:49 +0530

    Modified function file

---
+++ shell_scripts/local_var.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+function print(){
+   local name=$1
+    echo $name
+}
+name=max
+echo $name:Before
+
+print hello
+
+echo $name:After
+
+# All variables are global in shell scripts.
+# here name is a global variable and can be accessed anywhere in script.
+# if we want that the variable of a function should not change or it remain local we have to use local.
+
